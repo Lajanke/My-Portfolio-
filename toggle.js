9064dd1dbd17f71907097f2d5d8c09cb63c447fa
Lajanke <l.j.kenny@hotmail.com>
2020-03-05 23:43:52 +0000

    changed to 1 page scroller

--- toggle.js
+++ toggle.js
@@ -4,3 +4,12 @@ const navBarLinks = document.getElementsByClassName("navbar-links")[0];
 toggle.addEventListener("click", () => {
     navBarLinks.classList.toggle("active");
 });
+
+var shiftWindow = function() {
+    if ((window.innerHeight + window.pageYOffset) < document.body.scrollHeight) {
+        scrollBy(0, -45); 
+    }
+    navBarLinks.classList.toggle("active");
+};
+if (location.hash) shiftWindow();
+window.addEventListener("hashchange", shiftWindow);
