d330c14384a523bac78051bcd21012776397a63c
Lajanke <l.j.kenny@hotmail.com>
2020-03-17 16:03:23 +0000

    graduation countdown added

--- toggle.js
+++ toggle.js
@@ -14,3 +14,28 @@ var shiftWindow = function () {
 if (location.hash) shiftWindow();
 window.addEventListener("hashchange", shiftWindow);
 
+/*countdown*/
+
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+let countDown = new Date('Jun 12, 2020 17:00:00').getTime(),
+    x = setInterval(function() {    
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+      //do something later when date is reached
+      //if (distance < 0) {
+      //  clearInterval(x);
+      //  'IT'S MY BIRTHDAY!;
+      //}
+
+    }, second)
