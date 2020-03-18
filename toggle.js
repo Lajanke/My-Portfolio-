261ef322cc6ab82671298938f5fa2fc13fc90f46
Lajanke <l.j.kenny@hotmail.com>
2020-03-18 01:19:37 +0000

    1 bug fixed on toggle. X reverts to hamburger on menu click.

--- toggle.js
+++ toggle.js
@@ -6,19 +6,29 @@ const toggleicon = document.getElementById("toggleicon");
 
 toggle.addEventListener("click", () => {
     navBarLinks.classList.toggle("active");
-    toggleicon.classList.toggle("fa-bars");
+    toggleicon.classList.toggle("fa-bars", "fa-times");
     toggleicon.classList.toggle("fa-times");
 });
-
 
 /*Scrolling*/
 
 var shiftWindow = function () {
-    if ((window.innerHeight + window.pageYOffset) <= document.body.scrollHeight) {
+    var number = Math.floor(pageYOffset);
+    console.log(number);
+    console.log(pageYOffset);
+    if (number === (Math.floor(pageYOffset - 45))) {
+        scrollBy(0, 0);
+        number = Math.floor(pageYOffset);
+    }
+    else if ((window.innerHeight + window.pageYOffset) <= document.body.scrollHeight) {
         scrollBy(0, -45);
+        number = Math.floor((pageYOffset));
+        console.log(number);
     }
     navBarLinks.classList.toggle("active");
+    toggleicon.classList.toggle("fa-times");
 };
+
 if (location.hash) shiftWindow();
 window.addEventListener("hashchange", shiftWindow);
 
