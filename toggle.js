c799deb21699f459c9fc04242f702bb78517ee7e
Lajanke <l.j.kenny@hotmail.com>
2020-03-24 14:58:00 +0000

    Updated Graduation date and about page

--- toggle.js
+++ toggle.js
@@ -21,12 +21,12 @@ var shiftWindow = function () {
         scrollBy(0, 0);
         number = Math.floor(pageYOffset);
     }
-    else if ((window.innerHeight + window.pageYOffset) <= document.body.scrollHeight) {
-        scrollBy(0, -45);*/
+    else if ((window.innerHeight + window.pageYOffset) >= document.body.scrollHeight) {
+        scrollBy(0, -15);
         
        // number = Math.floor((pageYOffset));
        // console.log(number);
-    //}
+    }*/
     navBarLinks.classList.toggle("active");
     toggleicon.classList.toggle("fa-times");
 };
@@ -38,27 +38,28 @@ window.addEventListener("hashchange", shiftWindow);
 /*countdown*/
 
 const second = 1000,
-    minute = second * 60,
-    hour = minute * 60,
-    day = hour * 24;
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
 
-let countDown = new Date('Jun 12, 2020 17:00:00').getTime(),
-    x = setInterval(function () {
+let countDown = new Date('Jun 19, 2020 17:00:00').getTime(),
 
-        let now = new Date().getTime(),
-            distance = countDown - now;
+x = setInterval(function () {
 
-        document.getElementById('days').innerText = Math.floor(distance / (day)),
-            document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-            document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+    let now = new Date().getTime(),
+    distance = countDown - now;
 
-        //do something later when date is reached
-        //if (distance < 0) {
-        //  clearInterval(x);
-        //  'IT'S MY BIRTHDAY!;
-        //}
+    document.getElementById('days').innerText = Math.floor(distance / (day)),
+    document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+    document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+    document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
 
-    }, second)
+    //do something later when date is reached
+    //if (distance < 0) {
+    //  clearInterval(x);
+    //  Graduation!;
+    //}
+
+}, second)
 
 
