c503881d469c1a6c55071cd210aff9f08c79f9af
Lajanke <l.j.kenny@hotmail.com>
2020-07-03 15:51:44 +0100

    adds an avatar image.

--- toggle.js
+++ toggle.js
@@ -5,7 +5,7 @@ const navBarLinks = document.getElementsByClassName("navbar-links")[0];
 const toggleicon = document.getElementById("toggleicon");
 
 toggle.addEventListener("click", () => {
-    navBarLinks.classList.toggle("active");
+    navBarLinks.classList.toggle("active" );
     toggleicon.classList.toggle("fa-bars", "fa-times");
     toggleicon.classList.toggle("fa-times");
 });
@@ -13,20 +13,6 @@ toggle.addEventListener("click", () => {
 /*Scrolling*/
 
 var shiftWindow = function () {
-    
-    /*var number = Math.floor(pageYOffset);
-    console.log(number);
-    console.log(pageYOffset);
-    if (number === (Math.floor(pageYOffset - 45))) {
-        scrollBy(0, 0);
-        number = Math.floor(pageYOffset);
-    }
-    else if ((window.innerHeight + window.pageYOffset) >= document.body.scrollHeight) {
-        scrollBy(0, -15);
-        
-       // number = Math.floor((pageYOffset));
-       // console.log(number);
-    }*/
     navBarLinks.classList.toggle("active");
     toggleicon.classList.toggle("fa-times");
 };
